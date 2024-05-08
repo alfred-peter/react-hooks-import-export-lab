112fd6c318ddb4f208054233d634a3e8df478b90
alfred-peter <alfredkilonzo90@gmail.com>
2024-05-08 09:47:56 +0300

    completed labs

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,11 @@ function NavBar() {
   return (
     <nav>
       <a href="#home">I'm a link!</a>
+      <a href="#home">Home</a>
+      <a href="#home">About</a>
+
     </nav>
   );
 }
+
+export default NavBar;
